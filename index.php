2830bf8594e772c9cfff7980746f0f4c1dae406f
Rau261977 <141036372+Rau261977@users.noreply.github.com>
2024-07-09 20:54:29 -0300

    ok

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
     <div class="container-fluid py-4" aria-label="Imagen de fondo con textura arrugada" style="background-image: url('<?php echo get_template_directory_uri(); ?>/imagenes/arrugado.webp'); background-size: cover; background-repeat: no-repeat; background-position: center; min-height: 100vh;">
 
-
+        accccccccccccccccccccccccccccccccccccc
 
         <div class="gap-2 grow grow:hover text-center">
             <a href="https://api.whatsapp.com/send?phone=5492216182490&text=Hola,%20quiero%20más%20información" aria-label="Enlace para consultar información en WhatsApp" target="_blank" class="btn btn-primary" style="border-radius:50px;" role="button">Consulte ya!</a>
