567e18208e8885bbd5312d6f68ad0099dfe3782f
Rau261977 <141036372+Rau261977@users.noreply.github.com>
2024-07-24 10:53:58 -0300

    ok

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
     <div class="container px-5">
         <div class="row text-center align-items-center justify-content-center flex-column flex-sm-row">
             <div class="col-auto">
-                <a href="https://api.whatsapp.com/send?phone=5492216182490&text=Hola,%20quiero%20más%20información" target="_blank" rel="noopener noreferrer" class="whatsapp-icon" style="display: inline-block;">
+                <a href="https://api.whatsapp.com/send?phone=5492216182490&text=Hola,%20quiero%20más%20información" target="_blank" rel="noopener noreferrer" class="whatsapp-icon">
                     <span>Contacta por WhatsApp</span>
-                    <img src="<?php echo get_template_directory_uri(); ?>/imagenes/whatsapp-icon.png" alt="Icono de WhatsApp" style="margin-left: 5px;">
+                    <img src="<?php echo get_template_directory_uri(); ?>/imagenes/whatsapp-icon.png" alt="Icono de WhatsApp">
                 </a>
             </div>
             <div class="col-auto">
